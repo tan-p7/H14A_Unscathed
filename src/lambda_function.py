daa48f7544429cabe6982dc23ed942be2b9c8b43
Shreya Kamath <z5599327@ad.unsw.edu.au>
2026-03-12 23:18:25 +1100

    Added health check: including checking dynamodb connectivity

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,8 +1,24 @@
 import json
+import boto3
+from db import dynamodb_table
+
+# Import functions and constants that perform the core data processing
+from helper_functions import build_response
+from constants import JSON_TYPE
+
+# Initialise URL constants
+BASE_URL = '/api/despatch'
+HEALTH_CHECK_PATH = BASE_URL + '/health'
 
 def lambda_handler(event, context):
-  # TODO implement
-  return {
-    'statusCode': 200,
-    'body': json.dumps('Hello from lambda!')
-  }
+  http_method = event.get('httpMethod')
+  path = event.get('path')
+  if http_method == 'GET' and path == HEALTH_CHECK_PATH:
+    try:
+      dynamodb_table.table_status #FIXXXXXXX
+      response = build_response(200, JSON_TYPE, 'Service is operational')
+
+    except Exception as e:
+      print('Error:', e)
+      response = build_response(400, JSON_TYPE, 'Error processing request')
+    return response
