cc7c573ae63324cd92ce1d7d9d2ca7b6731aaf80
Anika Tahsin <anikatahsinn0@gmail.com>
2026-04-07 11:56:15 +1000

    add all page routes to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import Create from './pages/Create'
+import ViewDespatch from './pages/ViewDespatch'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/despatch/:id" element={<ViewDespatch />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
