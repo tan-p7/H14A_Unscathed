7d8b84a8f3593d0f29569ff17661907f3cd02c7b
Anika Tahsin <anikatahsinn0@gmail.com>
2026-04-07 12:11:13 +1000

    feat: protect all routes with PrivateRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,17 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Create from './pages/Create'
 import ViewDespatch from './pages/ViewDespatch'
+import PrivateRoute from './components/privateroute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/despatch/:id" element={<ViewDespatch />} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/create" element={<PrivateRoute><Create /></PrivateRoute>} />
+        <Route path="/despatch/:id" element={<PrivateRoute><ViewDespatch /></PrivateRoute>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
