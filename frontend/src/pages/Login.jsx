2d9cddfc51997162c1dc396e332169d49b91de37
Anika Tahsin <anikatahsinn0@gmail.com>
2026-04-20 17:37:34 +1000

    add login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const handleSubmit = () => {
+        console.log(email, password)
+        }
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="email" placeholder="Email" value={email}
+                onChange={(event) => setEmail(event.target.value)}/>
+            <input type="password" placeholder="Password" value={password}
+                onChange={(event) => setPassword(event.target.value)}/>
+            <button onClick={handleSubmit}>Log in</button>
+        </div>
+    )
+}
