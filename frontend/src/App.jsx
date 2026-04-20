90f81429dd4bc3746090c8dd649ff22593b00e92
Anika Tahsin <anikatahsinn0@gmail.com>
2026-04-20 20:39:59 +1000

    Additional page templates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ export  default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>} />
+        <Route path="/login" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
       </Routes>
     </BrowserRouter>
