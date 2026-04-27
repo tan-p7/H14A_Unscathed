a6857867126be7cca493b00c31cf3c130acc4ff2
Anika Tahsin <anikatahsinn0@gmail.com>
2026-04-28 00:56:14 +1000

    New AWS invoke url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/atlas': {
-        target: 'https://y1j7xv2ua6.execute-api.us-east-1.amazonaws.com/v2',
+        target: 'https://7ng4l9r5f2.execute-api.us-east-1.amazonaws.com/v2',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/atlas/, '')
       }
